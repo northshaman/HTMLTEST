2a0e1da1e58cf46d5904edd4024b34e450e6f889
northshaman <north-shaman@mail.ru>
2017-05-03 17:01:34 +0500

    go!

---
+++ myscript.js
@@ -0,0 +1,10 @@
+jQuery('document').ready(function(){
+jQuery('body').append('<a href="http://google.com">Перейти в Гугл!</a>');
+jQuery('div').remove();
+
+var p1;
+
+//pClone = jQuery('p').clone();
+//jQuery('body').append(pClone);
+
+});
