d3a7e73299b398c48c7aac76ccaea301614b89e5
northshaman <north-shaman@mail.ru>
2017-05-05 11:00:32 +0500

    05052017

--- myscript.js
+++ myscript.js
@@ -1,10 +1,24 @@
 jQuery('document').ready(function(){
 jQuery('body').append('<a href="http://google.com">Перейти в Гугл!</a>');
-jQuery('div').remove();
 
-var p1;
+jQuery('input').on('keyup',function(){
+  var value1,value2;
 
+  value1=parseInt(jQuery('#text1').val());
+  value2=parseInt(jQuery('#text2').val());
+
+jQuery('#exit').html(value1*value2);
+
+});
 //pClone = jQuery('p').clone();
 //jQuery('body').append(pClone);
-
+var test1;
+test1=prompt('Dratuti','123');
+jQuery(text1).val(test1);
+//test1 = confirm('Delete');
+if(test1==123){
+  alert("koko!");
+} else {
+  alert("bad");
+}
 });
